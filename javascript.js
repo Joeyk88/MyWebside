3a3fbd04bf0d94a7973056987acfaa16b789cff9
Joeyk88 <127742375+Joeyk88@users.noreply.github.com>
2023-10-03 13:18:49 +0200

    Ready for gallery

---
+++ javascript.js
@@ -0,0 +1,37 @@
+//overskrift på de elementer jeg er med mit arbejde//
+const text= document.querySelector(".sec-text");
+const textLoad =() =>{setTimeout(() =>{text.textContent ="Software Udvikler";}
+, 0);
+setTimeout(() => { text.textContent ="DJ";}, 4000);
+setTimeout(() =>{textContent = "Freelancer";},8000);}
+textLoad();
+setInterval
+(textLoad,12000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//1. Make a costumer template to sign in for information
+
+//2. Make a system for bookking a DJ: Prize for how many hours and per.hour
+
+//3. Where, when(date) the DJ is going a play
+
+//4. How much will the costumer have for the set-up = Speakers, bottom boxes/subwoofer and light systems
