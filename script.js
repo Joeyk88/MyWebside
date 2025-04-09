4dc1b733a2c698849ce38de95eac82aedf5d4776
joeykoldingkristensen <joeyk666@hotmail.com>
2025-04-09 17:49:52 +0200

    update

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //overskrift på de elementer jeg er med mit arbejde//
+document.addEventListener("DOMContentLoaded", () => {
+
 const text= document.querySelector(".sec-text");
 const textLoad =() =>{setTimeout(() =>{text.textContent ="Software Udvikler";}
 , 0);
@@ -9,44 +11,23 @@ textLoad();
 setInterval
 (textLoad,12000);
 
-//hvordan man åbner mine tabs//
-
-const tablinks = document.getElementsByClassName("tab-links");
-const tabcontents = document.getElementsByClassName("tab-contents");
-
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
+//åbner mine tabs//
 
 
+  const tabLinks = document.querySelectorAll(".tab-links");
+  const tabContents = document.querySelectorAll(".tab-contents");
 
+    tabLinks.forEach(link => {
+     link.addEventListener("click", (e) => {
+      const tabName = link.dataset.tab;
 
+      // Fjern alle aktive klasser
+      tabLinks.forEach(l => l.classList.remove("active-link"));
+      tabContents.forEach(c => c.classList.remove("active-tab"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//1. Make a costumer template to sign in for information
-
-//2. Make a system for bookking a DJ: Prize for how many hours and per.hour
-
-//3. Where, when(date) the DJ is going a play
-
-//4. How much will the costumer have for the set-up = Speakers, bottom boxes/subwoofer and light systems
+      // Tilføj aktive klasser
+      link.classList.add("active-link");
+      document.getElementById(tabName).classList.add("active-tab");
+    });
+  });
+});
