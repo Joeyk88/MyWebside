7e7a86fb61641171c65b6700450fe8ce1eabf4d1
joeykoldingkristensen <joeyk666@hotmail.com>
2025-04-11 12:04:28 +0200

    ny update

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 //overskrift på de elementer jeg er med mit arbejde//
 document.addEventListener("DOMContentLoaded", () => {
 
-/* const text= document.querySelector(".sec-text");
-const textLoad =() =>{setTimeout(() =>{text.textContent ="Software Udvikler";}
-, 0);
-//tidlinje mellem de 2 nye elementer og intervaler//
-setTimeout(() => { text.textContent ="DJ";}, 4000);
-setTimeout(() =>{textContent = "Freelancer";},8000);}
-textLoad();
-setInterval
-(textLoad,12000); */
+const text = document.querySelector(".sec-text");
+
+  const textLoad = () => {
+    setTimeout(() => {
+      text.textContent = "Web Developer";
+    }, 0);
+
+    setTimeout(() => {
+      text.textContent = "Software Developer";
+    }, 4000);
+
+    setTimeout(() => {
+      text.textContent = "Freelancer Developer";
+    }, 8000);
+  };
+
+  textLoad();
+  setInterval(textLoad, 12000);
 
 //åbner mine tabs//
-
-
   const tabLinks = document.querySelectorAll(".tab-links");
   const tabContents = document.querySelectorAll(".tab-contents");
 
